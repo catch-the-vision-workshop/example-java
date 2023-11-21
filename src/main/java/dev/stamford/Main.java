fc8444b9534bac8dfa3f5022bd4fd48fbdfe245d
Lxkas <alohasunshineday@yahoo.com>
2023-11-21 23:05:30 +0700

    basic calculator skeleton

--- src/main/java/dev/stamford/Main.java
+++ src/main/java/dev/stamford/Main.java
@@ -1,7 +1,43 @@
 package dev.stamford;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Basic Calculator!");
+
+        // Input for the first number
+        System.out.print("Enter first number: ");
+        double firstNumber = scanner.nextDouble();
+
+        // Input for the second number
+        System.out.print("Enter second number: ");
+        double secondNumber = scanner.nextDouble();
+
+        // Input for the operator
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        // Initialize the variable to store the result
+        double result = 0;
+
+        // Perform calculation based on the operator using if-else statements
+        if (operator == '+') {
+            // TODO: Implement addition logic
+        } else if (operator == '-') {
+            // TODO: Implement subtraction logic
+        } else if (operator == '*') {
+            // TODO: Implement multiplication logic
+        } else if (operator == '/') {
+            // TODO: Implement division logic (Remember to check for division by zero!)
+        } else {
+            System.out.println("Invalid operator!");
+            return;
+        }
+
+        // Display the result
+        System.out.println("Result: " + result);
     }
 }
